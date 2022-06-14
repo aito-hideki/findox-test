7428f04898c1a0e09b6cab52f96109659fa4446c
Aito Hideki <aito.hideki@gmail.com>
2022-06-14 13:25:08 -0400

    feat(document viewer): added document viewer options component

---
+++ src/components/DocumentsViewer/Options.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-btn
+    class="documents-viewer-options"
+    color="primary"
+    label="Options"
+    unelevated
+    @click.stop
+  >
+    <q-menu
+      class="documents-viewer-options__menu"
+      self="top middle"
+      max-height="none"
+    >
+      <q-card>
+        <q-card-section>
+          <template
+            v-for="field in fields"
+            :key="field"
+          >
+            <q-checkbox
+              v-model="visibleFields"
+              class="q-mb-xs"
+              :val="field.value"
+              :label="field.label"
+              size="xs"
+            />
+            <q-separator class="q-my-xs" />
+          </template>
+          <div class="flex justify-between q-mt-sm">
+            <q-btn
+              v-close-popup
+              label="Save"
+              color="primary"
+              unelevated
+              no-caps
+              @click="save"
+            />
+            <q-btn
+              v-close-popup
+              label="Cancel"
+              color="primary"
+              unelevated
+              no-caps
+              @click="cancel"
+            />
+            <q-btn
+              v-close-popup
+              label="Reset"
+              color="primary"
+              unelevated
+              no-caps
+              @click="reset"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-menu>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const emit = defineEmits<{
+  (event: 'update:visible', value: string[]): void
+}>()
+const props = defineProps<{
+  visible: string[]
+  fields: { label: string, value: string }[]
+}>()
+
+const visibleFields = ref([] as string[])
+
+// Excute operations including save, cancel, reset
+// only when those buttons are clicked
+
+const save = () => {
+  emit('update:visible', [...visibleFields.value])
+}
+
+const cancel = () => {
+  visibleFields.value = [...props.visible]
+}
+
+const reset = () => {
+  emit('update:visible', [...props.fields.map(({ value }) => value)])
+}
+
+// Always synchronize internal value with prop
+watch(() => props.visible, () => {
+  visibleFields.value = [...props.visible]
+}, { immediate: true })
+</script>
+
+<style lang="scss">
+.documents-viewer-options__menu {
+  width: 300px;
+}
+</style>
