c3721bf0a9ae734e8063e3c72d3e4d400422dd11
Aito Hideki <aito.hideki@gmail.com>
2022-06-14 13:25:49 -0400

    feat(document viewer): used document viewer in the app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-layout view="hHh Lpr fFf">
+    <q-page-container>
+      <q-page class="q-px-lg q-py-xl">
+        <DocumentsViewer :deals="deals" />
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { deals } from '@/consts/deals'
+</script>
